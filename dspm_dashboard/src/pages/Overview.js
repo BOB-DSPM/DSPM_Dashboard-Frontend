a61933c10133e16e4990fe1d60ba55891a0d032b
Ianthinus <y-j-h@naver.com>
2025-09-30 17:36:21 +0900

    perf: Update Overview

--- dspm_dashboard/src/pages/Overview.js
+++ dspm_dashboard/src/pages/Overview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import KPI from '../components/cards/KPI';
-import PieChartComponent from '../components/charts/PieChartComponent';
+import PieChartComponent from '../components/charts/BarChartComponent';
 import { Shield, Database, AlertTriangle, FileText } from 'lucide-react';
 import { issuesBySeverity, dataClassification } from '../data';
 
