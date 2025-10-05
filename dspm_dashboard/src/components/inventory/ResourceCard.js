e83cb66d2be31e167127e7db30be995cb7fb3c85
Ianthinus <y-j-h@naver.com>
2025-10-05 17:34:24 +0900

    pref: delete check

--- dspm_dashboard/src/components/inventory/ResourceCard.js
+++ dspm_dashboard/src/components/inventory/ResourceCard.js
@@ -39,13 +39,6 @@ const ResourceCard = ({ resource, onClick, isSelected }) => {
           <div className="font-medium text-gray-900 truncate">{resource.name}</div>
           <div className="text-sm text-gray-500">{resource.typeLabel}</div>
         </div>
-        {isSelected && (
-          <div className="w-5 h-5 bg-primary-500 rounded-full flex items-center justify-center">
-            <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg>
-          </div>
-        )}
       </div>
     </div>
   );
