b32be3d4674ebae4cd63e31561fe8a09658777b8
com_nyang <108343303+wonjjani@users.noreply.github.com>
2025-10-15 17:43:43 +0900

    Remove StrictMode from root render
    Removed React.StrictMode to prevent duplicate useEffect execution in development.

--- dspm_dashboard/src/index.js
+++ dspm_dashboard/src/index.js
@@ -1,18 +1,15 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './setupSafeJson'; 
+import './setupSafeJson'; // JSON.parse 패치
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 개발모드 중복 useEffect 실행 방지: StrictMode 제거
+root.render(<App />);
+
+// 성능 측정 필요하면 함수 전달
 reportWebVitals();
