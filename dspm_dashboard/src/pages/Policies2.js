783e9931268c3c5295674df4f4da4ae335e636d4
Ianthinus <y-j-h@naver.com>
2025-10-15 19:09:44 +0900

    change url

--- dspm_dashboard/src/pages/Policies2.js
+++ dspm_dashboard/src/pages/Policies2.js
@@ -13,8 +13,8 @@ import soc2Logo from './logo/soc2.png';
 import pipaLogo from './logo/pipa.png';
 
 
-const API_BASE = 'http://211.44.183.248:9000/compliance';
-const AUDIT_API_BASE = 'http://211.44.183.248:9000/auditor';
+const API_BASE = 'http://211.44.183.248:8003';
+const AUDIT_API_BASE = 'http://211.44.183.248:8003';
 
 const Policies2 = () => {
   const [frameworks, setFrameworks] = useState([]);
