04fbf53f359e79399e10b971c048e64079e244c4
com_nyang <108343303+wonjjani@users.noreply.github.com>
2025-10-15 19:27:19 +0900

    Update index.js

--- dspm_dashboard/src/index.js
+++ dspm_dashboard/src/index.js
@@ -1,14 +1,18 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './setupSafeJson'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// 개발모드 중복 useEffect 실행 방지: StrictMode 제거
-root.render(<App />);
-
-// 성능 측정 필요하면 함수 전달
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
